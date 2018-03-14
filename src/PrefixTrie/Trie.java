f4edefc7262a61098799f79d5699c94b23ca3ffa
Aidagots <gotovceva_1999@mail.ru>
2018-03-14 11:03:31 +0300

    Initial commit

---
+++ src/PrefixTrie/Trie.java
@@ -0,0 +1,145 @@
+package PrefixTrie;
+
+import java.util.*;
+
+public class Trie {
+
+    class TrieNode {
+        char c;
+        TrieNode parent;
+        HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
+        boolean isLeaf;
+
+        TrieNode() {
+        }
+
+        TrieNode(char c) {
+            this.c = c;
+        }
+    }
+
+    TrieNode root;
+    private ArrayList<String> words;
+    private TrieNode prefixRoot;
+    private String curPrefix;
+
+    public Trie() {
+        root = new TrieNode();
+        words = new ArrayList<String>();
+    }
+
+
+    public void insert(String word) {
+        HashMap<Character, TrieNode> children = root.children;
+
+        TrieNode crntparent;
+
+        crntparent = root;
+
+
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            TrieNode t;
+            if (children.containsKey(c)) {
+                t = children.get(c);
+            } else {
+                t = new TrieNode(c);
+                t.parent = crntparent;
+                children.put(c, t);
+            }
+
+            children = t.children;
+            crntparent = t;
+
+
+            if (i == word.length() - 1)
+                t.isLeaf = true;
+        }
+    }
+
+
+    public boolean search(String word) {
+        TrieNode t = searchNode(word);
+        return t != null && t.isLeaf;
+    }
+
+
+    public boolean startsWith(String prefix) {
+        return searchNode(prefix) != null;
+    }
+
+    TrieNode searchNode(String str) {
+        Map<Character, TrieNode> children = root.children;
+        TrieNode t = null;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (children.containsKey(c)) {
+                t = children.get(c);
+                children = t.children;
+            } else {
+                return null;
+            }
+        }
+
+        prefixRoot = t;
+        curPrefix = str;
+        words.clear();
+        return t;
+    }
+
+
+
+    void wordsFinderTraversal(TrieNode node, int offset) {
+
+
+        if (node.isLeaf) {
+
+
+            TrieNode altair;
+            altair = node;
+
+            Stack<String> hstack = new Stack<String>();
+
+            while (altair != prefixRoot) {
+
+                hstack.push(Character.toString(altair.c));
+                altair = altair.parent;
+            }
+
+            StringBuilder wrd = new StringBuilder(curPrefix);
+
+            while (!hstack.empty()) {
+                wrd.append(hstack.pop());
+            }
+
+
+            words.add(wrd.toString());
+
+        }
+
+        Set<Character> kset = node.children.keySet();
+
+        Iterator itr = kset.iterator();
+        ArrayList<Character> aloc = new ArrayList<Character>();
+
+        while (itr.hasNext()) {
+            Character ch = (Character) itr.next();
+            aloc.add(ch);
+
+        }
+
+
+
+        for (Character anAloc : aloc) {
+            wordsFinderTraversal(node.children.get(anAloc), offset + 2);
+        }
+
+    }
+
+    public void delete(String word) {
+
+    }
+
+}
---
+++ src/Test/TrieTest.java
@@ -0,0 +1,32 @@
+package Test;
+
+import PrefixTrie.Trie;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+class TrieTest {
+
+    @Test
+    void insert_search() {
+        Trie prefixTree;
+
+        prefixTree = new Trie();
+        prefixTree.insert("GOING");
+        prefixTree.insert("GONG");
+        prefixTree.insert("PAKISTAN");
+        prefixTree.insert("SHANGHAI");
+        prefixTree.insert("GONDAL");
+        prefixTree.insert("GODAY");
+        prefixTree.insert("GODZILLA");
+
+        assertTrue("GONG" , prefixTree.search("GONG"));
+        assertFalse("KROL" , prefixTree.search("KROL"));
+    }
+
+
+    @Test
+    void delete() {
+
+    }
+}
